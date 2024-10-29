6fafb1d8be8b70cd78f2151ec5502a2a775d5134
loran <laurent.muller@wizbii.com>
2024-10-29 14:28:23 +0100

    Initial Commit

---
+++ src/lib/Header.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	type HeaderProps = { name: string; fake_name?: string };
+	let { name, fake_name }: HeaderProps = $props();
+</script>
+
+<h1 class="text-3xl font-bold">Hello from our {name}</h1>
+
+{#if fake_name}
+	<p>Fake name: {fake_name}</p>
+{:else}
+	<p>No fake name provided</p>
+{/if}
