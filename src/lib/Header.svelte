865cc77d3fa8814ccb2bd07bea80cdb52a9c27be
loran <laurent.muller@wizbii.com>
2024-10-30 15:03:02 +0100

    feat(form): :sparkles: Add simple stepper + form checking

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
-	type HeaderProps = { name: string; fake_name?: string };
-	let { name, fake_name }: HeaderProps = $props();
+	import type { Snippet } from 'svelte';
+
+	type HeaderProps = {
+		name?: string;
+		fake_name?: string;
+	};
+	let { fake_name, name }: HeaderProps = $props();
 </script>
 
-<h1 class="text-3xl font-bold">Hello from our {name}</h1>
+<h1 class="text-3xl font-bold">
+	<span>
+		Hello {name ?? 'User'}
+	</span>
+</h1>
+
+<!-- {@render secondChild()} -->
 
 {#if fake_name}
 	<p>Fake name: {fake_name}</p>
-{:else}
-	<p>No fake name provided</p>
 {/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,120 @@
 <script lang="ts">
+	import Header from '$lib/Header.svelte';
+	import { error } from '@sveltejs/kit';
+
+	type FormStep = {
+		id: keyof FormState;
+		question: string;
+		type: 'text' | 'date' | 'color';
+		errorMessage: string;
+	};
+
+	type FormSteps = Array<FormStep>;
+
+	const QUESTIONS: FormSteps = [
+		{ id: 'name', question: `What's your name?`, type: 'text', errorMessage: 'Name is required' },
+		{
+			id: 'birthday',
+			question: `What's your birthday?`,
+			type: 'date',
+			errorMessage: 'Birthday is required'
+		},
+		{
+			id: 'color',
+			question: `What's your favorite color?`,
+			errorMessage: 'Birthday is required',
+			type: 'color'
+		}
+	];
+
+	type FormState = {
+		name: string;
+		birthday: string;
+		color: string;
+		step: number;
+		error: Error;
+		answers: Record<string, string> | null;
+	};
+
+	type Error = null | {
+		message: string;
+	};
+
+	let formState: FormState = $state({
+		name: '',
+		birthday: '',
+		color: '',
+		step: 0,
+		error: null,
+		answers: null
+	});
+
+	let displayStepIndex = $derived(formState.step + 1);
+	let formEnded = $derived(formState.step >= QUESTIONS.length);
+
+	function nextStep(id: keyof FormState) {
+		if (formState[id] === '') {
+			formState = {
+				...formState,
+				error: { message: QUESTIONS.find((q) => q.id === id)?.errorMessage ?? '' }
+			};
+			return;
+		}
+
+		formState = {
+			...formState,
+			answers: { ...formState.answers, [id]: formState[id] as string },
+			error: null,
+			step: ++formState.step
+		};
+	}
+
+	$inspect(formState);
 </script>
+
+<main>
+	<Header name={formState.name}></Header>
+
+	{#if formEnded}
+		{@render thankYou()}
+	{:else}
+		{@render stepTitle(displayStepIndex)}
+	{/if}
+
+	{#each QUESTIONS as question, step (question.id)}
+		{#if step === formState.step}
+			{@render formStep(question)}
+		{/if}
+	{/each}
+</main>
+
+{#snippet formStep({ id, question, type }: FormStep)}
+	<div>
+		<label for={id}>{question}</label>
+
+		<input {type} {id} bind:value={formState[id]} />
+
+		{#if formState.error}
+			{@render error(formState.error.message)}
+		{/if}
+	</div>
+	<button type="submit" onclick={() => nextStep(id)}>Next</button>
+{/snippet}
+
+{#snippet thankYou()}
+	<h1>Thank you!</h1>
+{/snippet}
+
+{#snippet error(message: string)}
+	<p class="error">{message}</p>
+{/snippet}
+
+{#snippet stepTitle(index: number)}
+	<h1>Step {index}</h1>
+{/snippet}
+
+<style>
+	.error {
+		color: red;
+	}
+</style>
